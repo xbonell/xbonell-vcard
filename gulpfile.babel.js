f52fd51219874eb66646f1ca544eed4fef6aabce
Xavier Bonell <mail@xbonell.com>
2025-07-22 16:00:46 +0200

    Updated dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,14 @@
 
 import pkg from './package.json';
 import autoprefixer from 'autoprefixer';
+import babelify from 'babelify';
 import browser from 'browser-sync';
 import browserify from 'browserify';
 import buffer from 'vinyl-buffer';
 import cssnano from 'cssnano';
 import combineMediaQuery from 'postcss-combine-media-query';
+import dartSass from 'sass';
+import envify from 'envify';
 import gulp from 'gulp';
 import image from 'gulp-image';
 import plugins from 'gulp-load-plugins';
@@ -14,27 +17,26 @@ import metalsmith from 'metalsmith';
 import layouts from 'metalsmith-layouts';
 import markdown from 'metalsmith-markdown';
 import permalinks from 'metalsmith-permalinks';
-import rimraf from 'rimraf';
+import { rimraf } from 'rimraf';
 import source from 'vinyl-source-stream';
 import yargs from 'yargs';
-import gulpSass from 'gulp-sass';
-import dartSass from 'sass';
-
-const sass = gulpSass(dartSass);
+import { hideBin } from 'yargs/helpers';
 
 // Load Gulp plugins
 const $ = plugins();
+const sass = $.sass(dartSass);
+const argv = yargs(hideBin(process.argv)).parse();
 
 // Look for args passed via cmd line parameters
-const PRODUCTION = !!(yargs.argv.production);
-const DEST = yargs.argv.dest;
+const PRODUCTION = !!argv.production;
+const DEST = argv.dest;
 
 // Main directories and Metalsmith configuration objects
 const dir = {
   base: __dirname + '/',
   lib: __dirname + '/src/_lib/',
   source: './src/',
-  dest: DEST || './dist/'
+  dest: DEST || './dist/',
 };
 
 const siteMeta = {
@@ -46,37 +48,29 @@ const siteMeta = {
   author: 'Xavier Bonell',
   contact: 'https://twitter.com/xbonell',
   domain: PRODUCTION ? 'http://localhost' : 'https://xbonell.com', // set domain
-  rootpath: PRODUCTION ? null : '/' // set absolute path (null for relative)
+  rootpath: PRODUCTION ? null : '/', // set absolute path (null for relative)
 };
 
 const templateConfig = {
   engine: 'haml',
   directory: dir.source + '_templates/',
   partials: dir.source + '_partials/',
-  default: 'default.haml'
+  default: 'default.haml',
 };
 
-// PostCSS filters 
-const processors = [
-  autoprefixer(),
-  combineMediaQuery(),
-  cssnano()
-];
+// PostCSS filters
+const processors = [autoprefixer(), combineMediaQuery(), cssnano()];
 
 // Create JS bundle
 // ----------------------------------------------------------------------------
 const bundle = () => {
   return browserify(`${dir.source}scripts/main.js`)
+    .transform(babelify, { presets: ['@babel/preset-env'] })
+    .transform(envify, { NODE_ENV: PRODUCTION ? 'production' : 'development' })
     .bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
     .pipe($.jslint())
-    .pipe($.if(!PRODUCTION, $.sourcemaps.init({
-      loadMaps: true
-    })))
-    .pipe($.if(PRODUCTION, $.babel({
-      presets: ['env']
-    })))
     .pipe($.if(PRODUCTION, $.uglify()))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write('./')))
     .pipe(gulp.dest(`${dir.dest}assets/js`));
@@ -85,31 +79,33 @@ const bundle = () => {
 // Delete the "dist" folder (this happens every time a build starts)
 // ----------------------------------------------------------------------------
 const clean = (done) => {
-  rimraf(`${dir.dest}**/*`, done);
-}
+  rimraf(`${dir.dest}**/*`);
+  done();
+};
 
 // Copy static files to root
 // ----------------------------------------------------------------------------
 const copy = () => {
-  return gulp.src(`${dir.source}_static/*.*`)
-    .pipe(gulp.dest(dir.dest));
-}
+  return gulp.src(`${dir.source}_static/*.*`).pipe(gulp.dest(dir.dest));
+};
 
-// Compile Sass into CSS and apply PostCSS filters
+// Compile Sass into CSS and apply filters
 // ----------------------------------------------------------------------------
 const css = () => {
-  return gulp.src(`${dir.source}scss/style.scss`)
+  return gulp
+    .src(`${dir.source}scss/style.scss`)
     .pipe($.if(!PRODUCTION, $.sourcemaps.init()))
-    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      sass({ outputStyle: $.if(PRODUCTION, 'compressed', 'expanded') }).on('error', sass.logError)
+    )
     .pipe($.postcss(processors))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(`${dir.dest}assets/css`));
-}
+};
 
 // Build HTML
 // ----------------------------------------------------------------------------
 const html = (done) => {
-
   metalsmith(dir.base)
     .clean(false)
     .source(`${dir.source}content/`)
@@ -122,107 +118,126 @@ const html = (done) => {
       if (err) throw err;
       done();
     });
-
-}
+};
 
 // Process images
 // ----------------------------------------------------------------------------
 const images = () => {
-  return gulp.src(`${dir.source}images/**/*`)
-    .pipe(image({
-      pngquant: false,
-      optipng: true,
-      zopflipng: false,
-      jpegRecompress: true,
-      jpegoptim: true,
-      mozjpeg: true,
-      gifsicle: true,
-      svgo: true,
-      concurrent: 8
-    }))
+  return gulp
+    .src(`${dir.source}images/**/*`)
+    .pipe(
+      image({
+        pngquant: false,
+        optipng: true,
+        zopflipng: false,
+        jpegRecompress: true,
+        jpegoptim: true,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 8,
+      })
+    )
     .pipe(gulp.dest(`${dir.dest}assets/images`));
-}
+};
 
 // Minify HTML
 // ----------------------------------------------------------------------------
 const minify = () => {
-  return gulp.src(`${dir.dest}**/*.html`)
-    .pipe($.if(PRODUCTION, $.htmlmin({
-      collapseWhitespace: true
-    })))
+  return gulp
+    .src(`${dir.dest}**/*.html`)
+    .pipe(
+      $.if(
+        PRODUCTION,
+        $.htmlmin({
+          collapseWhitespace: true,
+        })
+      )
+    )
     .pipe(gulp.dest(dir.dest));
-}
+};
 
 // Start a server with LiveReload to preview the site in
 // ----------------------------------------------------------------------------
 const server = (done) => {
   browser.init({
     server: dir.dest,
-    startPath: 'ca/'
+    startPath: 'ca/',
   });
   done();
-}
+};
 
 // Process SVG files and generate sprite
 // ----------------------------------------------------------------------------
 const svg = () => {
-
-  return gulp.src([`${dir.source}svg/*.svg`])
-    .pipe($.rename(function (path) {
-      path.basename = path.basename.replace(/__icon_prefix__/, '');
-      return path;
-    }))
-    .pipe($.svgmin(function (file) {
-      return {
-        // https://github.com/svg/svgo/tree/master/plugins
-        plugins: [{
-          cleanupIDs: {
-            remove: true,
-            minify: true
-          }
-        }, {
-          removeDoctype: true
-        }, {
-          removeComments: true
-        }, {
-          removeStyleElement: true
-        }, {
-          removeDimensions: true
-        }, {
-          cleanupNumericValues: {
-            floatPrecision: 2
-          }
-        }, {
-          removeAttrs: {
-            attrs: ['(fill|stroke|class|style)', 'svg:(width|height)']
-          }
-        }]
-        //, js2svg: { pretty: true } // uncomment for readability 
-      };
-    }))
+  return gulp
+    .src([`${dir.source}svg/*.svg`])
+    .pipe(
+      $.rename(function (path) {
+        path.basename = path.basename.replace(/__icon_prefix__/, '');
+        return path;
+      })
+    )
+    .pipe(
+      $.svgmin(function (file) {
+        return {
+          // https://github.com/svg/svgo/tree/master/plugins
+          plugins: [
+            {
+              cleanupIDs: {
+                remove: true,
+                minify: true,
+              },
+            },
+            {
+              removeDoctype: true,
+            },
+            {
+              removeComments: true,
+            },
+            {
+              removeStyleElement: true,
+            },
+            {
+              removeDimensions: true,
+            },
+            {
+              cleanupNumericValues: {
+                floatPrecision: 2,
+              },
+            },
+            {
+              removeAttrs: {
+                attrs: ['(fill|stroke|class|style)', 'svg:(width|height)'],
+              },
+            },
+          ],
+          //, js2svg: { pretty: true } // uncomment for readability
+        };
+      })
+    )
     .pipe($.svgstore())
     .pipe($.rename('sprite.svg'))
     .pipe(gulp.dest(`${dir.dest}assets/images`));
-}
+};
 
 // Watch for file changes
 // ----------------------------------------------------------------------------
 const watch = () => {
-  gulp.watch([`${dir.source}content/**/*.md`, `${dir.source}_templates/**/*.haml`])
+  gulp
+    .watch([`${dir.source}content/**/*.md`, `${dir.source}_templates/**/*.haml`])
     .on('change', gulp.series(html, minify, browser.reload));
 
-  gulp.watch([`${dir.source}scss/**/*.scss`])
-    .on('change', gulp.series(css, browser.reload));
+  gulp.watch([`${dir.source}scss/**/*.scss`]).on('change', gulp.series(css, browser.reload));
 
-  gulp.watch([`${dir.source}scripts/**/*.js`])
-    .on('change', gulp.series(bundle, browser.reload));
+  gulp.watch([`${dir.source}scripts/**/*.js`]).on('change', gulp.series(bundle, browser.reload));
 
-  gulp.watch([`${dir.source}svg/**/*.svg`])
-    .on('change', gulp.series(svg, browser.reload));
+  gulp.watch([`${dir.source}svg/**/*.svg`]).on('change', gulp.series(svg, browser.reload));
 
-  gulp.watch([`${dir.source}images/**/*.{gif,jpg,jpeg,png}`])
+  gulp
+    .watch([`${dir.source}images/**/*.{gif,jpg,jpeg,png}`])
     .on('change', gulp.series(images, browser.reload));
-}
+};
 
 // Build the "dist" folder by running all of the above tasks
 gulp.task('build', gulp.series(clean, gulp.parallel(bundle, css, html, images, svg, copy), minify));
